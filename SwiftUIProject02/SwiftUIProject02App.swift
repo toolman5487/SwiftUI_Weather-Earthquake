7aec1b69914e53f82d5a88a9489091635613380f
ToolManHsu <willy548798@gmail.com>
2025-06-12 11:00:06 +0800

    Initial Commit

---
+++ SwiftUIProject02/SwiftUIProject02App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProject02App.swift
+//  SwiftUIProject02
+//
+//  Created by Willy Hsu on 2025/6/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProject02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
